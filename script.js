df254f018b9c6a55e159dc98ab1c3878a8ff2071
Senthuran-S <senthuran0407@gmail.com>
2025-12-11 20:36:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const btn = document.getElementById('toggleTheme');
-if(btn){
-  btn.addEventListener('click', ()=>{
-    document.body.classList.toggle('dark');
-    document.body.classList.toggle('light');
-    btn.textContent = document.body.classList.contains('dark') ? '☀️ Light' : '🌙 Dark';
-  });
-}
